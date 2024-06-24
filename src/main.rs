bd617a22ab3854b855ed040c074e41cf8446709b
waimanho2765galois <waimanho2765galois@gmail.com>
2024-06-24 08:18:56 +0000

    Add comment

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,25 @@ fn index() -> &'static str {
     "OK"
 }
 
+
 #[get("/summarize")]
+// Rocket route that handles requests to '/summarize'
 async fn summarize_single_text() -> String {
+    // Retrieve OpenAI API key from environment variables
     let openai_api_key = match env::var("OPENAI_API_KEY") {
         Ok(api_key) => api_key,
         Err(e) => panic!("{}", e),
     };
     println!("{}", openai_api_key);
+
+    // Fetch most popular articles from NYTimes API
     let res = external_services::nytimes::get_most_popular(1).await;
     let articles = &res.results;
+
+    // Initialize prompt for summarization
     let mut prompt = String::from("Rewrite this text in summarized form.");
+
+    // Iterate over articles, appending abstracts to prompt
     for article in articles.iter() {
         let url = match &article.url {
             Some(value) => value,
@@ -34,23 +43,26 @@ async fn summarize_single_text() -> String {
         println!("{}: {}", url, &_abstract);
     }
 
+    // Call OpenAI API to summarize the text
     let summarized_response = external_services::openai::summarize(
         &prompt,
-        Some(1.0),
-        Some("gpt-4-turbo"),
-        Some(""),
-        Some(10000),
-        Some(","),
-        Some(true),
-        Some(true),
+        Some(1.0), // Temperature
+        Some("gpt-4-turbo"), // Model
+        Some(""), // System message
+        Some(10000), // Max tokens
+        Some(","), // Stop sequence
+        Some(true), // Summarize recursively
+        Some(true), // Return full text
     )
     .await;
 
+    // Return the summarized response
     summarized_response.clone()
 }
 
+
 #[launch]
 fn rocket() -> _ {
-    dotenv().expect(".env file not found");
+    let _ = dotenv();
     rocket::build().mount("/", routes![index, summarize_single_text])
 }
